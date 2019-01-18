5f4a9dd1520af0a24b8255e44240afdfc59bca22
Khalal Walker <khalal.walker@gmail.com>
2019-01-18 00:07:52 -0800

    create route from store picker page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-import "../css/style.css";
 import { getFunName } from "../helpers";
 class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Prevent form from submitting
+    event.preventDefault();
+    // 2. Get text from input
+    const storeName = this.myInput.current.value;
+    // 3. Change page to the store to whatever they entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a store</h2>
         <input
           type="text"
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
+          ref={this.myInput}
         />
         <button type="submit">Visit Store →</button>
       </form>
