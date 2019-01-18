5883402913434f8186f25124248c94807959e067
Khalal Walker <khalal.walker@gmail.com>
2019-01-18 15:18:17 -0800

    add final comment to state

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     const fishes = { ...this.state.fishes };
     // 2. Add new fish to the fishes variable
     fishes[`fish${Date.now()}`] = fish;
-
+    // 3. Set the new fishes object to state
     this.setState({ fishes });
   };
 
