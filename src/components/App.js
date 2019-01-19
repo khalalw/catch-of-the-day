ddc22dff6b50b0c0b128bf021724b1939af7e25c
Khalal Walker <khalal.walker@gmail.com>
2019-01-18 16:22:28 -0800

    create fish by pulling data from state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
   state = {
@@ -27,6 +28,11 @@ class App extends Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key, idx) => {
+              return <Fish key={key} details={this.state.fishes[key]} />;
+            })}
+          </ul>
         </div>
         <Order />
         <Inventory
