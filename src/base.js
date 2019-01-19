adeb11597377191beaeb8d3a2c0a76d2944dd30d
Khalal Walker <khalal.walker@gmail.com>
2019-01-19 14:04:18 -0800

    remove project name

--- src/base.js
+++ src/base.js
@@ -5,7 +5,6 @@ const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBGUe92SWr66b-DbbDwoSKBMsad3m5hVVs",
   authDomain: "catch-of-the-day-kw.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-kw.firebaseio.com",
-  projectId: "catch-of-the-day-kw",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
